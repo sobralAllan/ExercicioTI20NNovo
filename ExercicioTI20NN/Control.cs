2b7343744b9b6e1dbe26488fa130ac515210fcc6
ALLAN SOBRAL DA SILVA <allan.ssilva4@senacsp.edu.br>
2024-02-29 22:08:36 -0300

    Segundo Commit

--- ExercicioTI20NN/Control.cs
+++ ExercicioTI20NN/Control.cs
@@ -22,7 +22,8 @@ namespace ExerciciosTI20N
                               "1. Exercício 01\n" +
                               "2. Exercício 02\n" +
                               "3. Exercício 03\n" +
-                              "4. Exercício 04\n");
+                              "4. Exercício 04\n" +
+                              "5. Exercício 05\n");
         }//fim do menu
 
         public void Operacao()
@@ -40,6 +41,24 @@ namespace ExerciciosTI20N
                         break;
                     case 1:
                         Console.WriteLine(modelo.ExercicioUm());
+                        break;
+                    case 2:
+                        Console.WriteLine(modelo.ExercicioDois());
+                        break;
+                    case 3:
+                        Console.WriteLine("A soma dos números entre 1 e 100 é: " + modelo.ExercicioTres());
+                        break;
+                    case 4:
+                        Console.WriteLine("Os múltiplos de 5 entre 1 e 50 são: " + modelo.ExercicioQuatro());
+                        break;
+                    case 5:
+                        Console.WriteLine("Informe um número: ");
+                        int num = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine(modelo.ExercicioCinco(num));
+                        break;
+                    default:
+                        Console.WriteLine("Código digitado não é válido!");
                         break;
                 }//fim do método
             } while (opcao != 0);
--- ExercicioTI20NN/Model.cs
+++ ExercicioTI20NN/Model.cs
@@ -28,6 +28,62 @@ namespace ExerciciosTI20N
             return resultado;
         }//fim do método
 
+        //Faça um programa que imprima os números pares de 1 a 20.
+        public string ExercicioDois()
+        {
+            string resultado = "";
+
+            for(int i = 0; i < 20; i++)
+            {
+                if((i+1) % 2 == 0)
+                {
+                    resultado += "\n" + (i + 1);
+                }//fim do if
+            }//fim do for
+
+            return resultado;
+        }//fim do exercicio dois
+
+        //Exercício 3: Faça um programa que calcule a soma dos números de 1 a 100.
+        public int ExercicioTres()
+        {
+            int soma = 0;
+
+            for(int i=1; i <= 100; i++)
+            {
+                soma += i;
+            }//fim do for
+
+            return soma;
+        }//fim do método
+
+        //Exercício 4: Faça um programa que imprima os múltiplos de 5 de 1 a 50.
+        public string ExercicioQuatro()
+        {
+            string multiplo = "";
+            for(int i=1; i <= 50; i++)
+            {
+                if(i % 5 == 0)
+                {
+                    multiplo += "\n" + i;
+                }
+            }
+
+            return multiplo;
+        }//fim do método
+        
+        //Exercício 5: Faça um programa que peça ao usuário um número e imprima se é par ou ímpar.
+        public string ExercicioCinco(int num)
+        {
+            if(num % 2 == 0)
+            {
+                return num + " é par";
+            }
+            else
+            {
+                return num + "é ímpar";
+            }
+        }//fim do método
 
     }//fim da classe
 }//fim do projeto
